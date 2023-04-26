194e7ad5e0d79f2d845fd0872a9a86def81efb14
GuillermoSM <guillermosotomarquez5@gmail.com>
2023-04-26 12:48:10 -0600

    add style in layout

--- Layouts/MainLayout.tsx
+++ Layouts/MainLayout.tsx
@@ -22,7 +22,7 @@ export const MainLayout: FC<Props> = ({
 			</Head>
 
 			<main>
-				<div className="relative h-screen">
+				<div className="relative h-screen" style={{ minHeight: '100vh' }}>
 					<HeaderNav />
 
 					{children}
