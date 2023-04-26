c575cc61762436e84901af2d8df483b3f0020d29
GuillermoSM <guillermosotomarquez5@gmail.com>
2023-04-26 11:51:33 -0600

    response about me

--- pages/aboutme/index.tsx
+++ pages/aboutme/index.tsx
@@ -9,24 +9,29 @@ const index = () => {
 		<MainLayout title={'Sobre mi'} pageDescription={''}>
 			<div className="w-[90%] m-auto">
 				<h2 className="title-h2">Sobre mí</h2>
-				<div className="flex justify-center items-center ">
-					<div className="w-52 h-52 rounded-[50%] overflow-hidden relative mx-12">
+				{/* <div className="flex justify-center items-center "> */}
+				<div className="flex flex-col items-center lg:flex-row lg:justify-center ">
+					<div className="w-32 h-32 rounded-[50%] mb-4 overflow-hidden relative lg:w-52 lg:h-52 lg:mx-12">
 						<Image className="w-full absolute top-0" src={Profile} alt="" />
 					</div>
-					<p className="m-0 w-[35rem] text-start text-base">
-						Soy un aficionado a la tecnología y a la programación, actualmente
-						me encuentro estudiando FullStack JavaScript en la academia de Bedu
-						que finaliza en el mes de junio. Soy una persona alegre y positiva
-						siempre busco seguir aprendiendo para mejorar mis conocimientos y
-						mis habilidades. A continuación te dejo mi correo y algunas otros
-						lugares donde puedes contactarme.
+					<p className="m-0 w-full text-center text-base lg:w-[35rem] lg:text-start">
+						{/* <p className="m-0 w-[35rem] text-start text-base"> */}
+						Hola, mi nombre Guillermo me gradué como Ingeniero Electrónico del
+						Politécnico. Tengo experiencia como desarrollador Front-End, siempre
+						interesado por encontrar nuevos retos. Tengo el gusto de dedicarme a
+						esto que tanto me apasiona y esto me ha ayudado para aprender cada
+						día más y continuar preparándome para poder convertirme en un mejor
+						desarrollador
 					</p>
 				</div>
-				<h2 className="title-h2" style={{ marginBottom: '2rem' }}>
+				<h2
+					className="title-h2 hidden lg:block"
+					style={{ marginBottom: '2rem' }}
+				>
 					Contactame
 				</h2>
 
-				<div className="px-4 flex justify-center">
+				<div className="px-4 justify-center none hidden lg:flex">
 					<a
 						className="mx-8 text-4xl text-[#e48900] hover:text-[#c06014] "
 						target="_blank"
