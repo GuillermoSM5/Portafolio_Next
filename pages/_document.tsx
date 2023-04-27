12453ed4d1647cc86f1b1209a2d9ec7c3dc8fa23
GuillermoSM <guillermosotomarquez5@gmail.com>
2023-04-27 12:24:10 -0600

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</Head>
 			<body>
 				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 				<Main />
